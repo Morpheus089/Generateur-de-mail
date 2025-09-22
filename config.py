4e5682129320a9e9a37d0ab7910112ee15e266db
Tsukuyomi_089 <eole.noyon@gmail.com>
2025-09-22 20:32:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,1 @@
-API = "https://api.mail.tm"
 
-DB_HOST = "echoesofavalone.falixsrv.me"
-DB_PORT = 23003
-DB_USER = "u2234829_tURMMQXo48"
-DB_PASSWORD = "@w++01KK6IaVdjVUMYh=1pSf"
-DB_NAME = "s2234829_Generateur"
-
-USE_LOCAL_STORAGE_FALLBACK = True
