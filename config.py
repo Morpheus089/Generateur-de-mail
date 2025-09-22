0e0d18106340394d8b1b6526f167e82c42e51282
Morpheus089 <eole.noyon@gmail.com>
2025-09-22 20:27:55 +0200

    Version 2.0

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
 API = "https://api.mail.tm"
-SAVE_FILE = "account.json"
+
+DB_HOST = "echoesofavalone.falixsrv.me"
+DB_PORT = 23003
+DB_USER = "u2234829_tURMMQXo48"
+DB_PASSWORD = "@w++01KK6IaVdjVUMYh=1pSf"
+DB_NAME = "s2234829_Generateur"
+
+USE_LOCAL_STORAGE_FALLBACK = True
